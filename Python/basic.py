20d211d331dd3fef3cb29df7051d0e82dd6a5233
Bhupesh2975 <rajankushwaha0290@gmail.com>
2025-07-11 17:39:34 +0530

    commit

---
+++ Python/basic.py
@@ -0,0 +1,159 @@
+# # print("hello world")
+# # a="hello" 
+# # num = 10
+# # @= 4
+# # 2=2
+# # age =22
+# # age =23
+# # print("age")
+# # print("age")
+# # Break =22
+# # print =22
+# # print(print)
+
+# name = "Piyush"
+# print(name)
+# print("name dtyoes:-",type(name))
+# print("len of name:-",len(name))
+
+# ### indexing
+# print(name[0])
+# print(name[2])
+# print(name[len(name)-1])
+
+##slicing in python
+
+# name = "piyush"
+# print(name[0:3])
+
+
+# home work 
+
+### operation
+# name ="Bhupesh"
+# last = "kushwaha"
+# upper case = name ,upper()  ### uper function ka use upper case ke liye kerte h
+
+# print(upper_case)
+# lower_last_name=last.lower()
+# print(lower_last_name)
+
+# print(name.count("p"))
+
+# name="Bhupesh"
+# last="kushwaha"
+# print(name,title())
+# print(name.capitalline())
+# print(name+" "+last)
+
+# lst = [1,2,3,4,5, "Bhupesh", 2.3,] ##>>>>>list 
+#array or list >>>>
+#mutable 
+#duplicate values
+# ##hetro
+#order
+
+# array>>>>>>
+
+# lst = [1,2,3,4,5,6,7,]
+# print(lst)
+# print("type of list",type(lst))
+# print("length of list", len(lst))
+# lst.pop()
+# print(lst)
+# lst.insert(0,1000)
+# print(lst)
+# copy_lst =lst.copy()
+# print(copy_lst)
+# lst.reverse()
+# lst.remove(2.3)
+# print(lst)
+# lst.sort()
+# print(lst)
+# print(lst.count(2))
+# print(lst)
+# lst.clear
+
+# a,b,c=(1,2,3)
+# print(a)
+# print(b)
+# print(c)
+# a,b,c = 1,2,3
+# print(a)
+# print(b)
+# print(c)
+
+# my_dict = {
+#     "name":"Rohit",
+#     "class": "2nd year",
+#     "address": "jaipur",
+#     "roll no.": "22RU311071",
+# }
+
+# print("my first dict:-",my_dict)
+# print("my first dict:-",len(my_dict))
+# print("my first dict:-",type(my_dict))
+# print(my_dict["name"])
+# print(my_dict["class"])
+# print(my_dict["Roll number"])
+# print(my_dict["Address"])
+# my_dict["Address"]= "new jaipur"
+# my_dict = {
+#     "name":"Rohit",
+#     "class": "2nd year",
+#     "address": "jaipur","name":"rohit",
+#     "roll no.": "22RU311071"
+# }
+# my_dict["branch"]="cse"
+# print(my_dict)
+
+# access elements 6
+# lst = [1,2,3,4,5,[6,5,9],7,8]
+# print("full list", lst)
+# nested = lst[5.1]
+# print("nested lst:-", nested)
+# six = nested
+
+# print( my_dict.keys())
+# print( my_dict.values())
+# print( my_dict.items())
+
+# copy_dict = my_dict.copy()
+# print(copy_dict)
+
+# my_dict.clear()
+# print(my_dict)
+
+a=input("enter a number")
+b=input("enter second number")
+print(a*b)
+print(type(a))
+print(type(a))
+
+# type casting
+a =int(input("enter a number "))
+b =int(input("enter second number"))
+print(a*b)
+
+lst = [1,2,3,4,5,6,7,8]
+print("this is my list:-",lst)
+print("type of list:-",type(lst))
+print()
+
+# my_set = 
+# tuple = 
+# list
+# set
+# dict
+
+my_set = {1,2,3,4,5,6,7,8, "jai"}###>>>>>>>>
+print("this is my first set :-", my_set)
+print("length of my first set :-", len(my_set))
+print("type of my first set :-", type(my_set))
+
+# task>>>>>>>
+my_set.union()
+my_set.intersection()
+my_set.difference()
+
+
